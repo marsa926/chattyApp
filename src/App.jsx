2837390e0bf28f399631960da813427dcf60df8a
Sara Han <marsa926@gmail.com>
2017-02-07 16:43:04 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"},
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+  }
+
   render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
+
+      return (
+        <div className="wrapper">
+        <nav>
+        <h1>Chatty</h1>
+        </nav>
+      <MessageList />
+      <ChatBar />
+      </div>
+      );
+
   }
 }
 export default App;
+
+
